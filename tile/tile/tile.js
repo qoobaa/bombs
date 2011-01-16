aa5d4960cf88fd9a05e6dc68010133ef39d0d1c1
Jakub Kuźma <qoobaa+github@gmail.com>
2011-01-16 14:23:26 +0100

    added animationdirection

--- tile/tile/tile.js
+++ tile/tile/tile.js
@@ -12,9 +12,9 @@ YUI.add("tile", function (Y) {
 
             this.after("rowChange", this._afterRowChange);
             this.after("colChange", this._afterColChange);
-            this.after("hOffsetChange", this._afterHOffsetChange);
-            this.after("vOffsetChange", this._afterVOffsetChange);
-            this.after("directionChange", this._afterDirectionChange);
+            this.after("horizontalOffsetChange", this._afterHorizontalOffsetChange);
+            this.after("verticalOffsetChange", this._afterVerticalOffsetChange);
+            this.after("animationDirectionChange", this._afterAnimationDirectionChange);
         },
 
         _afterRowChange: function (event) {
@@ -25,72 +25,118 @@ YUI.add("tile", function (Y) {
             this.fire("engage", [this.get("row"), event.newVal]);
         },
 
-        _afterHOffsetChange: function (event) {
+        _afterHorizontalOffsetChange: function (event) {
+            if (event.newVal !== 0) {
+                this.set("verticalOffset", 0);
+            }
             if (event.newVal > 0 && this.get("direction") === Tile.RIGHT) {
                 this.fire("touch", [this.get("row") + 1, this.get("col")]);
             } else if (event.newVal < 0 && this.get("direction") === Tile.LEFT) {
                 this.fire("touch", [this.get("row") - 1, this.get("col")]);
             } else if (event.newVal > 1) {
-                this.setAttrs({ hOffset: event.newVal - 2, row: this.get("row") + 1 });
+                this.setAttrs({ horizontalOffset: event.newVal - 2, row: this.get("row") + 1 });
             } else if (event.newVal < 1) {
-                this.setAttrs({ hOffset: event.newVal + 2, row: this.get("row") - 1 });
+                this.setAttrs({ horizontalOffset: event.newVal + 2, row: this.get("row") - 1 });
             }
         },
 
-        _afterVOffsetChange: function (event) {
+        _afterVerticalOffsetChange: function (event) {
+            if (event.newVal !== 0) {
+                this.set("horizontalOffset", 0);
+            }
             if (event.newVal > 0 && this.get("direction") === Tile.DOWN) {
                 this.fire("touch", [this.get("row"), this.get("col") + 1]);
             } else if (event.newVal < 0 && this.get("direction") === Tile.UP) {
                 this.fire("touch", [this.get("row"), this.get("col") - 1]);
             } else if (event.newVal > 1) {
-                this.setAttrs({ vOffset: event.newVal - 2, col: this.get("col") + 1 });
+                this.setAttrs({ verticalOffset: event.newVal - 2, col: this.get("col") + 1 });
             } else if (event.newVal < 1) {
-                this.setAttrs({ vOffset: event.newVal + 2, col: this.get("col") - 1 });
+                this.setAttrs({ verticalOffset: event.newVal + 2, col: this.get("col") - 1 });
             }
         },
 
-        _afterDirectionChange: function (event) {
-            switch (event.newVal) {
-            case Tile.UP:
-                this.set("hOffset", 0);
-                break;
-            case Tile.RIGHT:
-                this.set("vOffset", 0);
-                break;
-            case Tile.DOWN:
-                this.set("hOffset", 0);
-                break;
-            case Tile.LEFT:
-                this.set("vOffset", 0);
-                break;
-            }
+        _afterAnimationDirectionChange: function (event) {
+            this.reset("animationFrame");
         },
 
         act: function () {
-            if (this.get("isMoving")) {
+            this.set("animationFrame", this.get("animationFrame") + 1);
+            if (this.get("moving")) {
                 this._move();
             }
+        },
+
+        _isMovedHorizontally: function () {
+            return Math.abs(this.get("horizontalOffset")) > this.get("speed");
+        },
+
+        _isMovedVertically: function () {
+            return Math.abs(this.get("verticalOffset")) > this.get("speed");
         },
 
         _move: function () {
             switch (this.get("direction")) {
             case Tile.UP:
-                this.set("vOffset", this.get("vOffset") - this.get("speed"));
+                if (this._isMovedHorizontally()) {
+                    this._stepCenter();
+                } else {
+                    this._stepUp();
+                }
                 break;
             case Tile.RIGHT:
-                this.set("hOffset", this.get("hOffset") + this.get("speed"));
+                if (this._isMovedVertically()) {
+                    this._stepCenter();
+                } else {
+                    this._stepRight();
+                }
                 break;
             case Tile.DOWN:
-                this.set("vOffset", this.get("vOffset") + this.get("speed"));
+                if (this._isMovedHorizontally()) {
+                    this._stepCenter();
+                } else {
+                    this._stepDown();
+                }
                 break;
             case Tile.LEFT:
-                this.set("hOffset", this.get("hOffset") - this.get("speed"));
+                if (this._isMovedVertically()) {
+                    this._stepCenter();
+                } else {
+                    this._stepLeft();
+                }
                 break;
             }
         },
 
+        _stepCenter: function () {
+            if (this.get("verticalOffset") < 0) {
+                this._stepDown();
+            } else if (this.get("verticalOffset") > 0) {
+                this._stepUp();
+            } else if (this.get("horizontalOffset") < 0) {
+                this._stepRight();
+            } else if (this.get("horizontalOffset") > 0) {
+                this._stepLeft();
+            }
+        },
+
+        _stepUp: function () {
+            this.setAttrs({ animationDirection: Tile.UP, verticalOffset: this.get("verticalOffset") - this.get("speed") });
+        },
+
+        _stepRight: function () {
+            this.setAttrs({ animationDirection: Tile.RIGHT, horizontalOffset: this.get("horizontalOffset") + this.get("speed") });
+        },
+
+        _stepDown: function () {
+            this.setAttrs({ animationDirection: Tile.DOWN, verticalOffset: this.get("verticalOffset") + this.get("speed") });
+        },
+
+        _stepLeft: function () {
+            this.setAttrs({ animationDirection: Tile.LEFT, horizontalOffset: this.get("horizontalOffset") - this.get("speed") });
+        },
+
         stop: function () {
-            this.setAttrs({ offset: [0, 0], isMoving: false });
+            this.setAttrs({ moving: false });
         },
 
         // INTERACTION - replace by events (defaultFn)?
@@ -142,20 +188,20 @@ YUI.add("tile", function (Y) {
                 }
             },
 
-            hOffset: {
+            horizontalOffset: {
                 validator: Y.Lang.isNumber
             },
 
-            vOffset: {
+            verticalOffset: {
                 validator: Y.Lang.isNumber
             },
 
             offset: {
                 getter: function () {
-                    return [this.get("hOffset"), this.get("vOffset")];
+                    return [this.get("horizontalOffset"), this.get("verticalOffset")];
                 },
                 setter: function (offset) {
-                    this.setAttrs({ hOffset: offset[0], vOffset: offset[1] });
+                    this.setAttrs({ horizontalOffset: offset[0], verticalOffset: offset[1] });
                 }
             },
 
@@ -163,12 +209,21 @@ YUI.add("tile", function (Y) {
                 validator: Y.Lang.isNumber
             },
 
-            isMoving: {
+            moving: {
                 validator: Y.Lang.isBoolean
             },
 
             direction: {
                 validator: "_validateDirection"
+            },
+
+            animationDirection: {
+                validator: "_validateDirection"
+            },
+
+            animationFrame: {
+                value: 0,
+                validator: Y.Lang.isNumber
             }
 
         },
